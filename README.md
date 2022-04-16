17dce8fcb237bce06386cd48e581b50024b1d995
ozerodb <50593663+ozerodb@users.noreply.github.com>
2022-04-16 22:22:19 +0200

    simplified usage
    - show_mask is now a  trackbar
    - unified image and device cli parameters

--- README.md
+++ README.md
@@ -2,23 +2,26 @@
 
 Little Python program to interactively find the best values for your HSV filter. Nothing new.
 
-*Example: finding color green*
+*Example: finding color green*<br>
 ![example](example.png)
 
 >*All credit for the `hsv_reference.png` image goes to [Jacob Rus](https://en.wikipedia.org/wiki/User:Jacobolus), I simply found it on [this Wikipedia page](https://en.wikipedia.org/wiki/HSL_and_HSV).*
 
 ## Usage
 
-The program accepts three optional arguments:
+The program accepts only one **optional** argument:
 
-- `--image=<src_image_path>` : if specified, uses the image in `<src_image_path>` instead of opening the webcam.
-- `--device=<device_index>` : index of the capturing device (default=0 for webcam).
-- `--show-mask` : if specified, also display the binary mask produced by the HSV filter.
+- `--source=<path_or_index>` : the path of the input image/video, or the index of the device (default 0 for webcam)
+
+Once launched, you can play with the trackbars until you find an acceptable HSV range for your application.
+
+Press q or Esc to quit.
 
 ### Examples
 
 ```bash
 python3 interactive-hsv-picker.py   # uses the webcam
-python3 interactive-hsv-picker.py --image='hsv_reference.png'   # uses the specified image
-python3 interactive-hsv-picker.py --image='hsv_reference.png' --show-mask   # also display the mask
+python3 interactive-hsv-picker.py --source='<device_idx>'    # uses the specified device
+python3 interactive-hsv-picker.py --source='hsv_reference.png'   # uses the specified image
+python3 interactive-hsv-picker.py --source='<input_video>.mp4'   # uses the specified video
 ```
--- interactive-hsv-picker.py
+++ interactive-hsv-picker.py
@@ -1,48 +1,87 @@
 import cv2
 import argparse
+import os.path
 import numpy as np
+
 
 def _nothing(x):
     pass
 
+
 def _get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--image', type=str, help='the path of the input image, when not using webcam')
-    parser.add_argument('--device', type=int, default=0, help='the index of the device to be used (default 0)')
-    parser.add_argument('--show-mask', default=False, action='store_true', help='if specified, also display the binary mask')
+    parser.add_argument(
+        "--source",
+        type=str,
+        default="0",
+        help="the path of the input image/video, or the index of the device (default 0 for webcam)",
+    )
     return parser.parse_args()
+
 
 def _main():
     # Parse arguments
     args = _get_args()
 
-    src = args.image if args.image else args.device
-    is_image = True if args.image else False
+    supported_image_ext = [
+        ".bmp",
+        ".dib",
+        ".jpeg",
+        ".jpg",
+        ".jpe",
+        ".jp2",
+        ".png",
+        ".webp",
+        ".pbm",
+        ".pgm",
+        ".ppm",
+        ".pxm",
+        ".pnm",
+        ".pfm",
+        ".sr",
+        ".ras",
+        ".tiff",
+        ".tif",
+        ".exr",
+        ".hdr",
+        ".pic",
+    ]
+
+    if not args.source:
+        print("Source cannot be empty!")
+        raise SystemExit
+
+    is_image = False
+    if args.source.isnumeric():
+        args.source = int(args.source)
+    elif os.path.splitext(args.source)[1] in supported_image_ext:
+        is_image = True
 
     # Instantiate the capture object
-    cap = cv2.VideoCapture(src)
+    cap = cv2.VideoCapture(args.source)
     if not cap.isOpened():
-        print(f'Error while opening source {src}')
+        print(f"Error while opening source {args.source}")
         raise SystemExit
 
     # Create a window
-    window_name = 'Interactive HSV picker'
+    window_name = "Interactive HSV picker"
     cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(window_name, 1280, 720)
     cv2.moveWindow(window_name, 100, 100)
 
     # Create trackbars
-    cv2.createTrackbar('Hmin', window_name, 0, 180, _nothing)
-    cv2.createTrackbar('Smin', window_name, 0, 255, _nothing)
-    cv2.createTrackbar('Vmin', window_name, 0, 255, _nothing)
-    cv2.createTrackbar('hMax', window_name, 0, 180, _nothing)
-    cv2.createTrackbar('sMax', window_name, 0, 255, _nothing)
-    cv2.createTrackbar('vMax', window_name, 0, 255, _nothing)
+    cv2.createTrackbar("Show mask", window_name, 0, 1, _nothing)
+    cv2.createTrackbar("Hmin", window_name, 0, 180, _nothing)
+    cv2.createTrackbar("Smin", window_name, 0, 255, _nothing)
+    cv2.createTrackbar("Vmin", window_name, 0, 255, _nothing)
+    cv2.createTrackbar("hMAX", window_name, 0, 180, _nothing)
+    cv2.createTrackbar("sMAX", window_name, 0, 255, _nothing)
+    cv2.createTrackbar("vMAX", window_name, 0, 255, _nothing)
 
-    # Set default value for Max HSV trackbars
-    cv2.setTrackbarPos('hMax', window_name, 180)
-    cv2.setTrackbarPos('sMax', window_name, 255)
-    cv2.setTrackbarPos('vMax', window_name, 255)
+    # Set default value for max HSV trackbars
+    cv2.setTrackbarPos("hMAX", window_name, 180)
+    cv2.setTrackbarPos("sMAX", window_name, 255)
+    cv2.setTrackbarPos("vMAX", window_name, 255)
 
     frame = None
     while True:
@@ -56,16 +95,17 @@ def _main():
             break
 
         # Get current positions of all trackbars
-        Hmin = cv2.getTrackbarPos('Hmin', window_name)
-        Smin = cv2.getTrackbarPos('Smin', window_name)
-        Vmin = cv2.getTrackbarPos('Vmin', window_name)
-        hMax = cv2.getTrackbarPos('hMax', window_name)
-        sMax = cv2.getTrackbarPos('sMax', window_name)
-        vMax = cv2.getTrackbarPos('vMax', window_name)
+        show_mask = cv2.getTrackbarPos("Show mask", window_name)
+        Hmin = cv2.getTrackbarPos("Hmin", window_name)
+        Smin = cv2.getTrackbarPos("Smin", window_name)
+        Vmin = cv2.getTrackbarPos("Vmin", window_name)
+        hMAX = cv2.getTrackbarPos("hMAX", window_name)
+        sMAX = cv2.getTrackbarPos("sMAX", window_name)
+        vMAX = cv2.getTrackbarPos("vMAX", window_name)
 
         # Set minimum and maximum HSV values to display
         lower = (Hmin, Smin, Vmin)
-        upper = (hMax, sMax, vMax)
+        upper = (hMAX, sMAX, vMAX)
 
         # Convert to HSV colorspace and perform thresholding
         hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
@@ -73,9 +113,9 @@ def _main():
         result = cv2.bitwise_and(frame, frame, mask=mask)
 
         # Concatenate the input and output frames
-        margin = np.ones((frame.shape[0], 5, 3), dtype = "uint8")*255
+        margin = np.ones((frame.shape[0], 5, 3), dtype="uint8") * 255
 
-        if args.show_mask:
+        if show_mask:
             mask = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
             output = cv2.hconcat([frame, margin, mask, margin, result])
         else:
@@ -88,6 +128,7 @@ def _main():
         k = cv2.waitKey(1)
         if k == 27 or k == 113:
             break
+
 
 if __name__ == "__main__":
     _main()
